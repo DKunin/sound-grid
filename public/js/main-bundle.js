67c69b59d76a5637d352c427034d21536d6891a2
Dmitri Kunin <whitemarten@gmail.com>
2016-04-07 21:06:14 +0300

    Minor fixes

--- public/js/main-bundle.js
+++ public/js/main-bundle.js
@@ -195,7 +195,7 @@
 	  'feMorphology', 'feOffset', 'fePointLight', 'feSpecularLighting',
 	  'feSpotLight', 'feTile', 'feTurbulence', 'filter', 'font', 'font-face',
 	  'font-face-format', 'font-face-name', 'font-face-src', 'font-face-uri',
-	  'foreignObject', 'glyph', 'glyphRef', 'hkern', 'image', 'line',
+	  'foreignObject', 'g', 'glyph', 'glyphRef', 'hkern', 'image', 'line',
 	  'linearGradient', 'marker', 'mask', 'metadata', 'missing-glyph', 'mpath',
 	  'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect',
 	  'set', 'stop', 'switch', 'symbol', 'text', 'textPath', 'title', 'tref',
