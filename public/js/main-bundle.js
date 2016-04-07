8864d139637590611e29e14964709e34cb485a95
Dmitry Kunin <dkunin@avito.ru>
2016-04-07 18:15:06 +0300

    Added test webspeech api

--- public/js/main-bundle.js
+++ public/js/main-bundle.js
@@ -8124,6 +8124,13 @@
 
 	var audio = document.getElementById('audioplayer');
 
+	function controlSound(a, name) {
+	    a.volume = 1;
+	    a.pause();
+	    a.src = '/media/sounds/' + name;
+	    a.play();
+	}
+
 	module.exports = function playSound(state, actions, event) {
 	    if (!audio.paused) {
 	        return false;
@@ -8132,9 +8139,21 @@
 	            return;
 	        }
 	        actions.setActive(event.target.dataset.id);
-	        audio.pause();
-	        audio.src = '/media/sounds/' + event.target.dataset.sound;
-	        audio.play();
+	        // if ('speechSynthesis' in window) {
+	        //     var msg = new SpeechSynthesisUtterance();
+	        //     msg.text = event.target.dataset.name;
+	        //     msg.volume = 1; // 0 to 1
+	        //     msg.rate = 1; // 0.1 to 10
+	        //     msg.pitch = 2; // 0 to 2
+	        //     msg.lang = 'ru-RU';
+	        //     speechSynthesis.speak(msg);
+
+	        //     msg.onend = function() {
+	        //         controlSound(audio, event.target.dataset.sound);
+	        //     };
+	        // } else {
+	        controlSound(audio, event.target.dataset.sound);
+	        //}
 	    }
 	};
 
